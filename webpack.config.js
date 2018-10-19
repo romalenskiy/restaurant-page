3a91f120e3c6e4570c0be6afeb9fb47aad6bf513
Roman Alenskiy <kuperman88@gmail.com>
2018-10-19 19:55:09 +0300

    Fixed paths

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -16,6 +17,18 @@ module.exports = {
         {
           test: /\.scss$/,
           use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+        },
+        {
+          test: /\.(png|jpg|gif)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                  name: '[path][name].[ext]',
+                  context: path.resolve(__dirname, "src/")
+              }
+          }
+          ]
         }
     ]
   },
@@ -23,6 +36,9 @@ module.exports = {
     new MiniCssExtractPlugin({
         filename: "[name].css",
         chunkFilename: "[id].css"
-    })
+    }),
+    new CopyWebpackPlugin([
+      {from: 'src/img', to: 'img'}
+    ])
   ]
 };
