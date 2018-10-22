5069b6d6d44537df46dfc42714ee4e1b7e1cf4d9
Roman Alenskiy <kuperman88@gmail.com>
2018-10-22 16:24:44 +0300

    Finished Menu tab

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import 'reset-css'
 import './app.scss'
 import * as About from './js_modules/about-tab'
+import * as Menu from './js_modules/menu-tab'
 
 const tabNavigation = document.querySelector('.tabs-navigation')
 tabNavigation.addEventListener('mousedown', tabToggle)
 
 function tabToggle(tab) {
     const tabs = {
-        'about': About.getTab()
+        'about': About.getTab(),
+        'menu': Menu.getTab()
     }
     const currentTab = tabs[tab.target.getAttribute('data-tab')]
 
---
+++ src/js_modules/menu-tab.js
@@ -0,0 +1,46 @@
+const getTab = function() {
+    const content = `
+        <div class="tab" id="menu-tab">
+            <h1>Select food</h1>
+            <div class="product">
+                <div class="product-image" id="chefburger"></div>
+                <h2 class="product-name">Chefburger</h2>
+            </div>
+            <div class="product">
+                    <div class="product-image" id="darkryeburger"></div>
+                    <h2 class="product-name">The dark rye burger</h2>
+            </div>
+            <div class="product">
+                    <div class="product-image" id="cheeseburger"></div>
+                    <h2 class="product-name">Cheeseburger</h2>
+            </div>
+            <div class="product">
+                    <div class="product-image" id="boxmasteroriginal"></div>
+                    <h2 class="product-name">Boxmaster original</h2>
+            </div>
+            <div class="product">
+                    <div class="product-image" id="twisteroriginal"></div>
+                    <h2 class="product-name">Twister original</h2>
+            </div>
+            <div class="product">
+                    <div class="product-image" id="hot-dog"></div>
+                    <h2 class="product-name">Hot-dog</h2>
+            </div>
+            <div class="product">
+                    <div class="product-image" id="hotwingsbucket-25"></div>
+                    <h2 class="product-name">25 hot wings bucket</h2>
+            </div>
+            <div class="product">
+                <div class="product-image" id="teriyakibites"></div>
+                <h2 class="product-name">Teriyaki bites</h2>
+            </div>
+            <div class="product">
+                <div class="product-image" id="lunchbox-5-300"></div>
+                <h2 class="product-name">Lunchbox 5 for 300</h2>
+            </div>
+        </div>
+    `
+    return content
+}
+
+export {getTab}
