c96e3c9186ea93f149f61ab6b885b72fabd286df
Roman Alenskiy <kuperman88@gmail.com>
2018-10-22 17:44:39 +0300

    Added Initial tab and tabs marking

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import * as About from './js_modules/about-tab'
 import * as Menu from './js_modules/menu-tab'
 import * as FindUs from './js_modules/find-us-tab'
 
+// Initial tab
+const tabsContent = document.querySelector('.tabs-content')
+tabsContent.innerHTML = About.getTab()
+markTab('about')
+
+
+// Tabs handler
 const tabNavigation = document.querySelector('.tabs-navigation')
 tabNavigation.addEventListener('mousedown', tabToggle)
 
@@ -13,8 +20,18 @@ function tabToggle(tab) {
         'menu': Menu.getTab(),
         'find-us': FindUs.getTab()
     }
-    const currentTab = tabs[tab.target.getAttribute('data-tab')]
+    const tabData = tab.target.getAttribute('data-tab')
+    const currentTab = tabs[tabData]
 
-    const tabsContent = document.querySelector('.tabs-content')
     tabsContent.innerHTML = currentTab
+    markTab(tabData)
+}
+
+function markTab(tab) {
+    const tabs = document.querySelectorAll('.tab-title')
+    tabs.forEach((tab) => {
+        tab.classList.remove('active')
+    })
+    const currentTab = document.querySelector(`.tab-title[data-tab="${tab}"]`)
+    currentTab.classList.add('active')
 }
